366331d89ea1e59678d498d46f4953cfa0203a7d
Gustaw <01178232@pw.edu.pl>
2024-02-24 12:38:11 +0100

    added parent Node id to node states

--- node_building_blocks.cpp
+++ node_building_blocks.cpp
@@ -1,7 +1,8 @@
 #include "node_building_blocks.h"
 
 #include <string>
-unsigned int ObjectID::ids_created = 0;
+
+unsigned int ObjectID::ids_created = 1;
 ObjectID::ObjectID() : id(ids_created++){};
 
 unsigned int ObjectID::get_id() const
@@ -9,7 +10,12 @@ unsigned int ObjectID::get_id() const
     return id;
 };
 
-NodeState::NodeState(unsigned int state_nr, std::string state_description) : state_nr(state_nr), state_description(state_description)
+bool ObjectID::operator==(const ObjectID other) const
+{
+    return (this->get_id() == other.get_id());
+}
+
+NodeState::NodeState(unsigned int state_nr, std::string state_description, ObjectID node_id) : state_nr(state_nr), state_description(state_description), node_id(node_id)
 {
 }
 
@@ -25,16 +31,25 @@ unsigned int NodeState::get_state_nr() const
 {
     return state_nr;
 }
+unsigned int NodeState::get_node_id() const
+{
+    return node_id.get_id();
+}
 std::string NodeState::get_description() const
 {
     return state_description;
 }
 
+std::string NodeState::get_string() const
+{
+    std::string str;
+    str += "N" + std::to_string(node_id.get_id()) + " ";
+    str += "s" + std::to_string(state_nr) + " ";
+    str += state_description;
+    return str;
+}
+
 bool NodeState::operator==(const NodeState other) const
 {
-    if ((this->state_nr) == (other.state_nr))
-    {
-        return true;
-    }
-    return false;
+    return ((this->state_nr == other.state_nr) && (this->node_id == other.node_id));
 }
--- node_building_blocks.h
+++ node_building_blocks.h
@@ -12,20 +12,25 @@ private:
 public:
     ObjectID();
     unsigned int get_id() const;
+    bool operator==(const ObjectID other) const;
 };
 
 class NodeState
 {
 private:
     unsigned int state_nr;
+    ObjectID node_id;
     std::string state_description;
 
 public:
-    NodeState(unsigned int state_nr, std::string state_description);
+    NodeState(unsigned int state_nr, std::string state_description, ObjectID node_id);
     void set_description(std::string state_description);
     void set_state_nr(unsigned int state_nr);
+    unsigned int get_node_id() const;
     unsigned int get_state_nr() const;
     std::string get_description() const;
+    std::string get_string() const;
+
     bool operator==(const NodeState other) const;
 };
 
