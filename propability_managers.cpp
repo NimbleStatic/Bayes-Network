3b25f07261279388c3af149da42465d6ce08fc65
Gustaw <01178232@pw.edu.pl>
2024-02-24 12:14:23 +0100

    implemented PropabilityCounter

---
+++ propability_managers.cpp
@@ -0,0 +1,164 @@
+#include <map>
+#include <memory>
+#include "propability_managers.h"
+#include <vector>
+#include <cmath>
+#include <random>
+#include <stdexcept>
+#include <algorithm>
+#include <string>
+#include <sstream>
+
+unsigned int PropabilityCounter::_get_full_count() const
+{
+    unsigned int full_count = 0;
+    for (const auto &key_value : outcome_propability_counts)
+    {
+        full_count += key_value.second;
+    };
+    return full_count;
+}
+
+bool PropabilityCounter::_is_state_in_system(std::shared_ptr<NodeState> state) const
+{
+    for (std::shared_ptr<NodeState> system_state : possible_states)
+    {
+        if (*system_state == *state)
+        {
+            return true;
+        };
+    }
+    return false;
+}
+
+PropabilityCounter::PropabilityCounter(std::vector<std::shared_ptr<NodeState>> states)
+{
+    add_states(states);
+}
+
+PropabilityCounter::PropabilityCounter()
+{
+}
+
+void PropabilityCounter::set_propability(std::shared_ptr<NodeState> state, float propability, unsigned int const_multiplier)
+{
+    unsigned int weight = std::round((propability * const_multiplier));
+    outcome_propability_counts[state] = weight;
+}
+void PropabilityCounter::update_propability(std::shared_ptr<NodeState> state, float propability, unsigned int const_multiplier)
+{
+    unsigned int weight = std::round((propability * const_multiplier));
+    update_propability(state, weight);
+}
+void PropabilityCounter::update_propability(std::shared_ptr<NodeState> state, unsigned int weight)
+{
+    outcome_propability_counts[state] += weight;
+}
+void PropabilityCounter::update_propability(std::shared_ptr<NodeState> state)
+{
+    update_propability(state, 1);
+}
+
+float PropabilityCounter::get_propability(std::shared_ptr<NodeState> state) const
+{
+    unsigned int prob_counter;
+    auto it = outcome_propability_counts.find(state);
+    if (it != outcome_propability_counts.end())
+    {
+        prob_counter = it->second;
+    }
+    else
+    {
+        // @TODO add custom exception no state in the state map
+        throw std::out_of_range("State not found in the state map");
+    };
+    unsigned int all_counts = _get_full_count();
+    float prob = static_cast<float>(prob_counter) / static_cast<float>(all_counts);
+    return prob;
+}
+std::vector<float> PropabilityCounter::get_propabilities(std::vector<std::shared_ptr<NodeState>> states) const
+{
+    std::vector<float> propabilities;
+    for (std::shared_ptr<NodeState> state : states)
+    {
+        propabilities.push_back(get_propability(state));
+    };
+    return propabilities;
+}
+std::shared_ptr<NodeState> PropabilityCounter::choose_state() const
+{
+
+    std::vector<float> propabilities = get_propabilities(possible_states);
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::discrete_distribution<int> prop_distribution(propabilities.begin(), propabilities.end());
+
+    std::size_t rand_ind = prop_distribution(gen);
+    std::shared_ptr<NodeState> rand_state = possible_states[rand_ind];
+    return rand_state;
+}
+
+bool PropabilityCounter::add_state(std::shared_ptr<NodeState> new_state, unsigned int state_count)
+{
+    if (!_is_state_in_system(new_state))
+    {
+        possible_states.push_back(new_state);
+        outcome_propability_counts[new_state] = state_count;
+        return true;
+    };
+    return false;
+}
+bool PropabilityCounter::add_state(std::shared_ptr<NodeState> new_state)
+{
+    return add_state(new_state, 1);
+}
+void PropabilityCounter::add_states(std::vector<std::shared_ptr<NodeState>> new_states)
+{
+    for (std::shared_ptr<NodeState> state : new_states)
+    {
+        add_state(state);
+    };
+}
+bool PropabilityCounter::delete_state(std::shared_ptr<NodeState> state)
+{
+    if (_is_state_in_system(state))
+    {
+        auto it = std::find(possible_states.begin(), possible_states.end(), state);
+        auto it_m = outcome_propability_counts.find(state);
+
+        if ((it != possible_states.end()) && (it_m != outcome_propability_counts.end()))
+        {
+            possible_states.erase(it);
+            outcome_propability_counts.erase(it_m);
+            return true;
+        }
+        else if (it == possible_states.end())
+        {
+            throw std::runtime_error(" there is state in system but it isnt findable");
+        }
+        else if (it_m == outcome_propability_counts.end())
+        {
+            throw std::runtime_error(" there is state in possible_states but not in propability_outocomes");
+        };
+    }
+    return false;
+}
+void PropabilityCounter::delete_states(std::vector<std::shared_ptr<NodeState>> states)
+{
+    for (std::shared_ptr<NodeState> state : states)
+    {
+        delete_state(state);
+    };
+}
+
+std::string PropabilityCounter::get_description() const
+{
+    std::ostringstream oss;
+    for (const auto key_val : outcome_propability_counts)
+    {
+        std::shared_ptr<NodeState> state = key_val.first;
+        oss << state->get_description() << " - count:" << key_val.second << ", propability:" << get_propability(state) << std::endl;
+    };
+    return oss.str();
+}
--- propability_managers.h
+++ propability_managers.h
@@ -1,23 +1,9 @@
 #ifndef PROPABILITY_MANAGERS_H
 #define PROPABILITY_MANAGERS_H
-#include <node_building_blocks.h>
+#include "node_building_blocks.h"
 #include <memory>
 #include <map>
 #include <vector>
-
-// class PropabilityRepresantation
-// {
-// private:
-//     unsigned int propability_count;
-//     std::shared_ptr<NodeState> outcome_state;
-
-// public:
-//     int get_propability_count();
-//     void set_propability_count(unsigned int new_count);
-//     void update_propability_count(unsigned int value);
-//     PropabilityRepresantation(std::shared_ptr<NodeState> outcome_state);
-//     std::shared_ptr<NodeState> get_state_referenced();
-// };
 
 class PropabilityCounter
 {
@@ -25,21 +11,32 @@ private:
     std::map<std::shared_ptr<NodeState>, unsigned int> outcome_propability_counts;
     std::vector<std::shared_ptr<NodeState>> possible_states;
 
-    unsigned int _get_full_count();
+    unsigned int _get_full_count() const;
+    bool _is_state_in_system(std::shared_ptr<NodeState> state) const;
 
 public:
-    void set_propability(std::shared_ptr<NodeState> state, float propability, unsigned int const_multiplier);
-    void update_propability(std::shared_ptr<NodeState> state, float propability, unsigned int const_multiplier);
-    void update_propability(unsigned int weight);
-    void update_propability();
-    float get_propability(std::shared_ptr<NodeState> state);
-    std::vector<float> get_propabilities(std::vector<std::shared_ptr<NodeState>> states);
-    std::shared_ptr<NodeState> choose_state();
+    PropabilityCounter(std::vector<std::shared_ptr<NodeState>> states);
+    PropabilityCounter();
 
+    void set_propability(std::shared_ptr<NodeState> state, float propability, unsigned int const_multiplier);
+
+    void update_propability(std::shared_ptr<NodeState> state, float propability, unsigned int const_multiplier);
+    void update_propability(std::shared_ptr<NodeState> state, unsigned int weight);
+    void update_propability(std::shared_ptr<NodeState> state);
+
+    float get_propability(std::shared_ptr<NodeState> state) const;
+    std::vector<float> get_propabilities(std::vector<std::shared_ptr<NodeState>> states) const;
+
+    std::shared_ptr<NodeState> choose_state() const;
+
+    // When adding states the default propability count is 1
+    bool add_state(std::shared_ptr<NodeState> new_state, unsigned int state_count);
     bool add_state(std::shared_ptr<NodeState> new_state);
-    void add_states(std::vector<std::shared_ptr<NodeState>> possible_states);
-    bool delete_state(std::shared_ptr<NodeState> new_state);
-    void delete_states(std::vector<std::shared_ptr<NodeState>> possible_states);
+    void add_states(std::vector<std::shared_ptr<NodeState>> new_states);
+    bool delete_state(std::shared_ptr<NodeState> state);
+    void delete_states(std::vector<std::shared_ptr<NodeState>> states);
+
+    std::string get_description() const;
 };
 
 class NodePropabilityTable
